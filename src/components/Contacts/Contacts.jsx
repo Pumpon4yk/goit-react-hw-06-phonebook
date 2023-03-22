cc8359eb41e4072b8ffa43c63161d721480d165e
Pumpon4yk <tt10392@gmail.com>
2023-03-23 00:15:33 +0100

    v1

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,51 @@
+import {
+  Container,
+  Label,
+  Input,
+  List,
+  Item,
+  PhoneNumber,
+  ButtonDelete,
+} from './Contacts.styled';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { getContacts, getStatusFilter } from 'redux/seletors';
+import { deleteContact } from 'redux/contactSlice';
+import { setFilter } from 'redux/filterSlice';
+
+const contactsFilter = (arr, name) =>{
+    if(name === '') return arr;
+    return arr.filter(e => e.name.toLowerCase().includes(name.toLowerCase()))
+  };
+
+const Contacts = () => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getStatusFilter);
+  const dispatch = useDispatch();
+  const visibleContacts = contactsFilter(contacts[0], filter);
+
+  const handleFilterChange = e => dispatch(setFilter(e.target.value));
+
+  return (
+    <Container>
+      <Label>
+        Find contacts by name
+        <Input value={filter} onChange={handleFilterChange} />
+      </Label>
+
+      <List>
+        {visibleContacts.map(({ id, name, number }) => (
+          <Item key={id}>
+            {name}:<PhoneNumber>{number}</PhoneNumber>
+            <ButtonDelete id={id} onClick={() => dispatch(deleteContact(id))}>
+              Delete
+            </ButtonDelete>
+          </Item>
+        ))}
+      </List>
+    </Container>
+  );
+};
+
+
+export default Contacts;
---
+++ src/redux/store.js
@@ -0,0 +1,31 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+import { persistedReducer } from './contactSlice';
+import { filterReducer } from './filterSlice';
+
+
+
+
+export const store = configureStore({
+  reducer: {
+    contacts: persistedReducer,
+    filter: filterReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);
